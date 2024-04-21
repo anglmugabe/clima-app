164d84ce253fd5152856cf3a2f0a3a594ffcb254
anglmugabe <anglmugabe@gmail.com>
2024-04-21 17:58:23 +0200

    Adding Api Intergration (Axios)

--- src/clima.js
+++ src/clima.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "cadc0335bc0tc128c364a2674f06oeee";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
