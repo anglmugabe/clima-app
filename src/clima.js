9a36e1be8e5ac1eb68cf1763c6588dea1bccf323
anglmugabe <anglmugabe@gmail.com>
2024-04-28 18:06:34 +0200

    Adding forecast API integration

--- src/clima.js
+++ src/clima.js
@@ -55,8 +55,16 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let days = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
-  let apiKey = "cadc0335bc0tc128c364a2674f06oeees";
+  let apiKey = "cadc0335bc0tc128c364a2674f06oeee";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
@@ -64,29 +72,31 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = " ";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast">
-                    <div class="row">
-                        <div class="col-2">
-                            <div class="weather-forecast-date">${day}
-                            </div>
-                            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/snow-day.png" alt=""
-                                width="42" />
-                            <div class="weather-forecast-temperatures">
-                                <span class="weather-forecast-temperature-max">
-                                    19°
-                                </span>
-                                <span class="weather-forecast-temperature-min">
-                                    8°
-                                </span>
-                            </div>
-                        </div>
-                    </div>`;
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+          <div class="col-2">
+            <div class="weather-forecast-date">${formatDay(day.time)}
+            </div>
+            
+            <img src= "${
+              day.condition.icon_url
+            }"/ class="weather-forecast-icon">
+            <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">
+                ${Math.round(day.temperature.maximum)}
+                </span>
+                <span class="weather-forecast-temperature-min">
+                   ${Math.round(day.temperature.minimum)}
+                </span>
+                </div>
+        </div>
+`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
